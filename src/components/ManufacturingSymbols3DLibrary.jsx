36eb4524005973b851bc63811025854f91924364
Roque Martinez <rmartinez@coreflexalliance.net>
2026-01-17 16:37:34 -0500

    Fix Manufacturing 3D assets import path (case-sensitive)

--- src/components/ManufacturingSymbols3DLibrary.jsx
+++ src/components/ManufacturingSymbols3DLibrary.jsx
@@ -1,6 +1,6 @@
 // ManufacturingSymbols3DLibrary.jsx
 import FloatingWindow from "./FloatingWindow";
-import ManufacturingSymbols3DLibraryContent from "../assets/Manufacturing-symbols 3D/ManufacturingSymbols3DLibraryContent";
+import ManufacturingSymbols3DLibraryContent from "../assets/manufacturing-symbols-3d/ManufacturingSymbols3DLibraryContent";
 
 export default function ManufacturingSymbols3DLibrary(props) {
   return (
