f6cd098ea87ed5f0856735a0ff6d6aa040d65981
Roque Martinez <rmartinez@coreflexalliance.net>
2026-01-29 12:07:43 -0500

    Fix AlarmLog drag via window manager

--- src/components/ManufacturingSymbols3DLibrary.jsx
+++ src/components/ManufacturingSymbols3DLibrary.jsx
@@ -1,10 +1,10 @@
-import FloatingWindow from "./FloatingWindow";
+import ShapeOfFloatingWindows from "./ShapeOfFloatingWindows";
 import ManufacturingSymbols3DLibraryContent from "./ManufacturingSymbols3DLibraryContent";
 
 export default function ManufacturingSymbols3DLibrary(props) {
   return (
-    <FloatingWindow {...props} title="Manufacturing Symbols 3D">
+    <ShapeOfFloatingWindows {...props} title="Manufacturing Symbols 3D">
       <ManufacturingSymbols3DLibraryContent />
-    </FloatingWindow>
+    </ShapeOfFloatingWindows>
   );
 }
