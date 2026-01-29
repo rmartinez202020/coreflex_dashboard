7f658928c996c279f2d38192d22d0d83f296e7f8
Roque Martinez <rmartinez@coreflexalliance.net>
2026-01-29 11:55:10 -0500

    Fix AlarmLog drag via window manager

--- src/components/HvacSymbols2DLibrary.jsx
+++ src/components/HvacSymbols2DLibrary.jsx
@@ -1,10 +1,10 @@
-import FloatingWindow from "./FloatingWindow";
+import ShapeOfFloatingWindows from "./ShapeOfFloatingWindows";
 import HvacSymbols2DLibraryContent from "./HvacSymbols2DLibraryContent";
 
 export default function HvacSymbols2DLibrary(props) {
   return (
-    <FloatingWindow {...props} title="HVAC Symbols 2D">
+    <ShapeOfFloatingWindows {...props} title="HVAC Symbols 2D">
       <HvacSymbols2DLibraryContent />
-    </FloatingWindow>
+    </ShapeOfFloatingWindows>
   );
 }
