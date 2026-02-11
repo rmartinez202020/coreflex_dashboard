bb03cfaa477d48229f9abed980c4fcd6356da1b6
Roque Martinez <rmartinez@coreflexalliance.net>
2026-02-11 14:01:54 -0500

    Frontend: show DI-5/DI-6 for ZHC1921 Device Manager

--- src/components/indicators/index.js
+++ src/components/indicators/index.js
@@ -1,12 +1,11 @@
-// src/components/indicators/index.js
-
 export { default as DraggableLedCircle } from "./DraggableLedCircle";
 export { default as DraggableStatusTextBox } from "./DraggableStatusTextBox";
 export { default as DraggableBlinkingAlarm } from "./DraggableBlinkingAlarm";
 export { default as DraggableStateImage } from "./DraggableStateImage";
 
-// ✅ Settings modals
+// ✅ NEW: Counter Input
+export { default as DraggableCounterInput } from "./DraggableCounterInput";
+
 export { default as IndicatorLightSettingsModal } from "./IndicatorLightSettingsModal";
 export { default as StatusTextSettingsModal } from "./StatusTextSettingsModal";
 export { default as BlinkingAlarmSettingsModal } from "./BlinkingAlarmSettingsModal";
-export { default as StateImageSettingsModal } from "./StateImageSettingsModal";
