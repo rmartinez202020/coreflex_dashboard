81ce67b5ffa5e6f07f56f5e556ed6252390b6b6f
Roque Martinez <rmartinez@coreflexalliance.net>
2026-04-09 13:53:21 -0400

    Fix PortalTopBar import path

--- src/components/display/DisplayOutputTextBoxStyle.jsx
+++ src/components/display/DisplayOutputTextBoxStyle.jsx
@@ -932,7 +932,7 @@ export default function DisplayOutputTextBoxStyle({
       lineHeight: 1,
       whiteSpace: "nowrap",
       position: "relative",
-      left: -12,
+      left: -16,
     }}
   >
     Control Action in Progress
