f23de9a3a66100109bfbfc0d3023a80ac23070aa
Roque Martinez <rmartinez@coreflexalliance.net>
2026-05-09 21:00:57 -0400

    Remove unused push button binding lookup routes

--- src/components/Sidebarleftwirelesstank.jsx
+++ src/components/Sidebarleftwirelesstank.jsx
@@ -23,17 +23,20 @@ export default function Sidebarleftwirelesstank({
         strokeLinecap="round"
         strokeLinejoin="round"
       >
+        {/* main tank */}
         <path d="M30 95 L135 125 L492 95 L492 405 L135 452 L30 385 Z" />
         <path d="M30 95 L135 125 L135 452" />
-        <path d="M135 125 L492 95" />
         <path d="M492 95 L492 405" />
         <path d="M30 385 L135 452 L492 405" />
         <path d="M30 91 L135 120 L492 90" />
         <path d="M30 95 L30 385" />
 
-        <path d="M135 125 L360 72 L492 95" />
+        {/* open top/rear edges */}
+        <path d="M30 95 L135 125 L360 72 L492 95" />
+        <path d="M135 125 L492 95" />
         <path d="M360 72 L360 325" strokeDasharray="7 8" opacity="0.65" />
 
+        {/* internal CAD lines */}
         <path
           d="M30 202 L135 235 L360 205 L492 240"
           strokeDasharray="7 8"
@@ -50,20 +53,28 @@ export default function Sidebarleftwirelesstank({
           opacity="0.55"
         />
 
-        <path d="M72 42 L180 28 L252 45 L143 61 Z" />
-        <path d="M72 42 L143 61 L143 123 L72 102 Z" />
-        <path d="M143 61 L252 45 L252 107 L143 123 Z" />
-        <path d="M67 37 L180 22 L257 40" />
-        <path d="M67 37 L139 56" />
-        <path d="M67 45 L139 64 L257 48" />
-        <path d="M72 102 L143 123 L252 107" />
-        <path d="M68 106 L139 130 L253 113" />
-        <path d="M139 130 L135 125" />
+        {/* top filter box - aligned to tank top */}
+        <path d="M72 48 L178 32 L252 50 L145 68 Z" />
+        <path d="M72 48 L145 68 L145 126 L72 106 Z" />
+        <path d="M145 68 L252 50 L252 108 L145 126 Z" />
 
-        <ellipse cx="104" cy="78" rx="13" ry="25" />
-        <ellipse cx="108" cy="79" rx="10" ry="22" />
-        <ellipse cx="112" cy="80" rx="6" ry="17" />
+        {/* top cap/lip */}
+        <path d="M66 42 L178 25 L258 44" />
+        <path d="M66 42 L140 63" />
+        <path d="M66 50 L140 71 L258 52" />
 
+        {/* bottom lip sitting on tank */}
+        <path d="M72 106 L145 126 L252 108" />
+        <path d="M68 112 L140 134 L254 116" />
+        <path d="M140 134 L135 125" />
+        <path d="M254 116 L252 108" />
+
+        {/* top round port */}
+        <ellipse cx="103" cy="82" rx="13" ry="24" />
+        <ellipse cx="107" cy="83" rx="10" ry="21" />
+        <ellipse cx="111" cy="84" rx="6" ry="16" />
+
+        {/* side latches */}
         <path d="M28 160 L45 166 L45 188 L28 183 Z" />
         <path d="M31 160 L38 150 L45 166" />
         <path d="M32 172 L42 175" />
@@ -76,6 +87,7 @@ export default function Sidebarleftwirelesstank({
         <path d="M111 216 L124 220" />
         <path d="M100 197 L106 189 L106 216 L100 223 Z" />
 
+        {/* lower round ports */}
         <ellipse cx="46" cy="360" rx="10" ry="23" />
         <ellipse cx="50" cy="360" rx="8" ry="20" />
         <ellipse cx="54" cy="360" rx="5" ry="15" />
@@ -84,6 +96,7 @@ export default function Sidebarleftwirelesstank({
         <ellipse cx="116" cy="395" rx="9" ry="21" />
         <ellipse cx="120" cy="395" rx="5" ry="16" />
 
+        {/* feet */}
         <path d="M140 452 L140 466 L178 461 L178 447" />
         <path d="M146 453 L146 461 L171 458 L171 449" />
         <path d="M432 413 L432 428 L468 423 L468 409" />
